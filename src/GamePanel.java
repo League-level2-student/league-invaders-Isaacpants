c7ac31d5474f1c480a79aca8ce35f625fe29566e
League Student <league@iMac-22.attlocal.net>
2019-09-10 16:48:32 -0700

    league invaders ip

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,59 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 
-public class GamePanel {
+import javax.swing.JPanel;
 
+public class GamePanel extends JPanel{
+	Font titleFont;
+	Font titleFont2;
+	 final int MENU = 0;
+	    final int GAME = 1;
+	    final int END = 2;
+	    int currentState = MENU;
+	   
+	    
+	    
+	    public GamePanel() {
+	    	 titleFont = new Font("Arial", Font.PLAIN, 48);
+	    	 titleFont2 = new Font("Arial", Font.PLAIN, 24);
+		}
+		void updateMenuState() {  }
+	    void updateGameState() {  }
+	    void updateEndState()  {  }
+	    void drawMenuState(Graphics g) { 
+	    	
+	    	g.setColor(Color.BLUE);
+	    g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT); 
+	    g.setFont(titleFont);
+	    g.setColor(Color.YELLOW);
+	    g.drawString("LEAGUE INVADERS", 10, 50);
+	    g.setFont(titleFont2);
+	    g.drawString("Press ENTER to Start",125, 350);
+	    g.drawString("Press SPACE for Instructions",90, 550);
+
+	    }
+	    void drawGameState(Graphics g) {  }
+	    void drawEndState(Graphics g)  {  
+	    	
+	      	g.setColor(Color.RED);
+		    g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT); 
+		    g.setFont(titleFont);
+		    g.setColor(Color.BLACK);
+		    g.drawString("GAME OVER", 100, 150);
+		    g.setFont(titleFont2);
+		    g.drawString("You Killed "+ " "+" enemies",125, 350);
+		    g.drawString("Press ENTER to Restart",115, 550);
+	    	
+	    }
+	@Override
+	public void paintComponent(Graphics g){
+		if(currentState == MENU){
+		    drawEndState(g);
+		}else if(currentState == GAME){
+		    drawGameState(g);
+		}else if(currentState == END){
+			drawMenuState(g);
+		}
+	}
 }
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 
 public class LeagueInvaders {
 JFrame jf;
+GamePanel gp = new GamePanel();
+
 public static final int HEIGHT =800; 
 public static final int WIDTH =500;
 public LeagueInvaders() {
@@ -14,7 +16,7 @@ public LeagueInvaders() {
 	li.setup();
 }
 	public void setup() {
-		
+	jf.add(gp);	
 		jf.setSize(WIDTH, HEIGHT);
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
